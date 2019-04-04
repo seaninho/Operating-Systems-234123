054e0e9723878be2263bbc4c7ba59985aa5d17b4
Unknown <seaninho10@gmail.com>
2019-04-04 21:40:28 +0300

    Repo creation
    Some initial files added

---
+++ Wet1/hw1_syscalls.c
@@ -0,0 +1,13 @@
+#include <linux/kernel.h>
+
+int​ ​sc_restrict​​ (​pid_t​ pid ,​int​ proc_restriction_level, scr* restrictions_list, ​int​ list_size) {
+
+}
+
+int​ ​set_proc_restriction​ (pid_t​ pid ,​int​ proc_restriction_level) {
+
+}
+
+int ​get_process_log (​pid_t ​ pid, ​int ​ size, fai* user_mem) {
+
+}
