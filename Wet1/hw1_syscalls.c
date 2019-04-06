f731de1582f4114bb76e72100d2367d139e805c7
Sean <seaninho10@gmail.com>
2019-04-07 00:02:00 +0300

    hw1_syscalls updated. sched.h added.

--- Wet1/hw1_syscalls.c
+++ Wet1/hw1_syscalls.c
@@ -1,13 +1,118 @@
 #include <linux/kernel.h>
+#include <linux/sched.h>
 
+// Description:
+//    Imposes the restrictions for the process with PID= ​pid ​ with respect to the given
+//    proc_restriciton_level and the ​restricitions_list.​ The restriction level of this process should
+//    be set to ​ proc_restriction_level​. In addition, from this point on, the log should record (at
+//    most) the last ​100 ​forbidden activities that this process has performed.
+// Parameters:
+//    @pid - The process for whom we would like to set the restrictions
+//    @proc_restriction_level - The process restriction level. From now on, every invocation of a
+//      system call which is configured in ​restrictions_list ​ that has a
+//      strictly greater ​restriction_theshold​ than ​proc_restriction_level
+//      should fail and will be recorded as a forbidden activity.
+//    @restrictions_list - A configuration list in which every item contains a system call
+//      number and its desired ​restriction_theshold​.
+//      Every system call that is not in this list should not be affected.
+//    @list_size - The number of elements in ​restricions_list ​. You may assume that
+//      this number always match the actual list size.
+//Return values:
+//    ● On success: return 0.
+//    ● On failure: return -1
+//        o If pid<0 errno should contain ​ESRCH
+//        o If no such process exists errno should contain ​ESRCH
+//        o If proc_restriction_level<0 or proc_restriction_level>2 errno should contain
+//          EINVAL
+//        o If size<0 errno should contain ​EINVAL
+//        o On memory allocation failure errno should contain ​ENOMEM
+//        o On memory copy failure errno should contain ​ENOMEM
+//        o On any other failure errno should contain ​EINVAL
 int​ ​sc_restrict​​ (​pid_t​ pid ,​int​ proc_restriction_level, scr* restrictions_list, ​int​ list_size) {
+  struct task_struct* p;
+  // Invalid pid
+  if (validate_pid(pid, p) == -1) {
+    return -ESRCH;
+  }
+  // Invalid restriction level
+  if (proc_restriction_level < 0 || proc_restriction_level > 2) {
+    return -EINVAL;
+  }
+  // Invalid size
+  if (size < 0){
+    return -EINVAL;
+  }
 
 }
 
+// Description:
+//    Sets a new restriction level for the process with PID= ​pid​. This method only sets a new
+//    restriction level regardless of the process’s restrictions list.
+//      ● If this method was called in parallel to the ​sc_restrict ​method, then the one that
+//        was performed last should takes effect.
+//      ● If this method was called more than once than its last invocation takes effect.
+// Parameters:
+//    @pid - The process for whom we would like to set the restrictions
+//    @proc_restriction_level - The process new restriction level
+//Return values:
+//    ● On success: return 0.
+//    ● On failure: return -1
+//        o If pid<0 errno should contain ​ESRCH
+//        o If no such process exists errno should contain ​ESRCH
+//        o If proc_restriction_level<0 or proc_restriction_level>2 errno should contain
+//          EINVAL
+//        o On any other failure errno should contain ​EINVAL
 int​ ​set_proc_restriction​ (pid_t​ pid ,​int​ proc_restriction_level) {
+  struct task_struct* p;
+  // Invalid pid
+  if (validate_pid(pid, p) == -1) {
+    return -ESRCH;
+  }
+  // Invalid restriction level
+  if (proc_restriction_level < 0 || proc_restriction_level > 2) {
+    return -EINVAL;
+  }
+
+  p -> proc_restriction_level = proc_restriction_level;
+  return 0;
+}
+
+// Description:
+//    Returns in ​user_mem​ the last ​size ​records of the forbidden activities of the process with
+//    PID = ​pid.
+//    ** Reading data from the log does not remove or change the log​. ** ​
+// Parameters:
+//    @pid The - process for whom we would like to set the restrictions
+//    @size The - number of records we wish to read from the log
+//    @user_mem - An address in the user space into which the method should write the result
+//Return values:
+//    ● On success: return 0.
+//    ● On failure: return -1
+//        o If pid<0 errno should contain ​ESRCH
+//        o If no such process exists errno should contain ​ESRCH
+//        o If size> actual number of records in the log, errno should contain ​EINVAL​.
+//        o If size<0 errno should contain ​EINVAL
+//        o On memory copy failure errno should contain ​ENOMEM
+//        o On any other failure errno should contain ​EINVAL
+int ​get_process_log (​pid_t ​ pid, ​int ​ size, fai* user_mem) {
+  struct task_struct* p;
+  if (validate_pid(pid, p) == -1) {
+    return -ESRCH;
+  }
 
 }
 
-int ​get_process_log (​pid_t ​ pid, ​int ​ size, fai* user_mem) {
-
+int validate_pid (pid_t pid, task_struct* p) {
+  // Invalid PID
+  if (pid < 0) {
+    return -1;
+  }
+  struct task_struct* tmp;
+  tmp = find_task_by_pid(pid);
+  // If p is NULL, there is no process with this pid.
+  if (tmp == NULL) {
+    return -1;
+  }
+  p = tmp;
+  return 0;
 }
--- Wet1/restrict.c
+++ Wet1/restrict.c
@@ -4,7 +4,7 @@
 
 void shift_left(struct task_struct *p) {
 	int i = 1 ;
-	for( ; i < p->violations ; i++ ){
+	for( ; i < p->violations ; i++ ) {
 		p->log_forbidden_activity[ i-1 ].syscall_num = p->log_forbidden_activity[ i ].syscall_num;
 		p->log_forbidden_activity[ i-1 ].syscall_restriction_threshold = p->log_forbidden_activity[ i ].syscall_restriction_threshold;
 		p->log_forbidden_activity[ i-1 ].proc_restriction_level = p->log_forbidden_activity[ i ].proc_restriction_level;
@@ -15,25 +15,25 @@ void shift_left(struct task_struct *p) {
 
 // recording forbidden activity - when current task privilege level is lower then the privilege level threshold of the involved system call syscall_number
 void recording_forbidden_activity (struct task_struct *p, int syscall_num, int threshold) {
-	if(p->violations = LOG_SIZE ){
+	if(p->violations = LOG_SIZE){
 		shift_left(p);
 	}
 	p->log_forbidden_activity[ p->violations ].syscall_num = syscall_num;
 	p->log_forbidden_activity[ p->violations ].syscall_restriction_threshold = threshold;
 	p->log_forbidden_activity[ p->violations ].proc_restriction_level = p->restriction_level;
 	p->log_forbidden_activity[ p->violations ].time = jiffies;
-	p->violations++;  
+	p->violations++;
 }
 
-// Checks whether the requested system call is a valid call 
-int is_valid(int syscall_num){
-	if ( current->feature == OFF ){
+// Checks whether the requested system call is a valid call
+int is_valid(int syscall_num) {
+	if (current->feature == OFF) {
 		return 1;
 	}
 	int i = 0;
-	for( ; i < current->restricions_list_size ; i++ ){
-		if( current->restricions_list[i].syscall_num == syscall_num){
-			if( current->restriction_level < current->restricions_list[i].restriction_threshold){
+	for( ; i < current->restricions_list_size ; i++ ) {
+		if(current->restricions_list[i].syscall_num == syscall_num) {
+			if(current->restriction_level < current->restricions_list[i].restriction_threshold) {
 				recording_forbidden_activity(current, syscall_num, restricions_list[i].restriction_threshold);
 				return 0;
 			}
@@ -41,5 +41,5 @@ int is_valid(int syscall_num){
 				return 1;
 			}
 	}
-	return 1;	
+	return 1;
 }
