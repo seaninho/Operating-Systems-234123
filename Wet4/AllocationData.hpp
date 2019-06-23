9fa7075103c58eb4d90440d59668f0fcb222abbf
Ayelet Israel <ayeletisrael3@gmail.com>
2019-06-23 14:17:37 +0300

    update for malloc_2

--- Wet4/AllocationData.hpp
+++ Wet4/AllocationData.hpp
@@ -1,11 +1,33 @@
 class AllocationData {
 public:
-   AllocationData(uint size) {
-      is_free = false;
-      requested_size = size;
-   }
+	AllocationData(uint size) {
+		_is_free = false;
+		_requested_size = size;
+		_effective_allocation = NULL;
+	}
 
+	void set_is_free(bool free){
+		_is_free = free;
+	}
+	void set_requested_size(uint size){
+		_requested_size = size;
+	}
+	void set_effective_allocation(void *addr){
+		_effective_allocation = addr;
+	}
+
+	bool is_free(){
+		return _is_free;
+	}
+	uint get_requested_size(){
+		return _requested_size;
+	}
+	void* get_effective_allocation(){
+		return _effective_allocation;
+	}
+	
 private:
-   bool is_free;
-   uint requested_size;
+   bool _is_free;
+   uint _requested_size;
+   void *_effective_allocation;
 }
--- Wet4/malloc_2.cpp
+++ Wet4/malloc_2.cpp
@@ -1,11 +1,46 @@
 #include <cstdlib.h>
 #include <unistd.h>
+#include <list>
+#include <iterator> 
+#include "AllocationData.hpp"
 
 #define MIN_SIZE 0
 #define MAX_SIZE 100000000
 
-void* malloc(size_t size) {
+list<AllocationData> allocationsHistory ;
 
+void* malloc(size_t size) {
+	if (size <= MIN_SIZE || size > MAX_SIZE)
+		return NULL;
+  
+	/*Search for enough space in released structures*/
+	
+	list<AllocationData>::iterator it; 
+    for(it = allocationsHistory.begin(); it != allocationsHistory.end(); ++it){
+		if(it.is_free() && it.get_requested_size()>=size){
+			it.set_is_free(false);
+			return it.get_effective_allocation();
+		}	
+	}
+  
+	/*Not enough space was found in the released structers*/
+	
+	//allocate for meta_data
+	AllocationData *meta_data = (AllocationData *)sbrk(sizeof(AllocationData));
+	if (meta_data == (void*)(-1))
+		return NULL;
+  
+	//allocate for effective_allocation
+	void *effective_allocation = sbrk(size);
+	if (effective_allocation == (void*)(-1))
+		return NULL;
+	
+	meta_data->set_is_free(false);
+	meta_data->set_requested_size(size);
+	meta_data->set_effective_allocation(effective_allocation);	
+	allocationsHistory.add(meta_data);
+	
+	return effective_allocation;
 }
 
 void* calloc(size_t size) {
